c3b4bf2d44f31e1cb66740105f3dbefc0c0a1c9b
Bruno Petrus <xpetrus@fi.muni.cz>
2025-06-25 16:08:07 +0200

    feat: enhance tunnel extraction process with size tracking

--- utilities/dataset/split_data.py
+++ utilities/dataset/split_data.py
@@ -49,6 +49,15 @@ def bbox_3d(img: NDArray) -> List[Tuple[int, int]]:
 def process(gt: NDArray, imgs: NDArray, time_dim: int = 0) -> List[Tuple[NDArray, NDArray]]:
     extracted_tunnels = []
 
+    # Keep track of largest size
+    largest_row_range = 0
+    largest_row_range_id = 0,0
+    largest_col_range = 0
+    largest_col_range_id = 0,0
+    largest_z_range = 0
+    largest_z_range_id = 0,0
+
+
     # Go through each time frame
     for t_idx in range(gt.shape[time_dim]):
         gt_timeslice = gt[t_idx]
@@ -62,21 +71,37 @@ def process(gt: NDArray, imgs: NDArray, time_dim: int = 0) -> List[Tuple[NDArray
             rows, cols, zs = bbox_3d(gt_timeslice == tunnel_id)
             extracted_tunnel_gt = gt_timeslice[rows[0]:rows[1]+1, cols[0]:cols[1]+1, zs[0]:zs[1]+1]
             extracted_tunnel_img = img_timeslice[rows[0]:rows[1]+1, cols[0]:cols[1]+1, zs[0]:zs[1]+1]
-
-            # TODO: padding
             extracted_tunnels.append(
                 (extracted_tunnel_gt, extracted_tunnel_img)
             )
+
+            if tunnel_id == 0:
+                continue  # Skip BG
+
+            if largest_row_range < rows[1]-rows[0]+1:
+                largest_row_range = rows[1]-rows[0]+1
+                largest_row_range_id = tunnel_id, t_idx
+            if largest_col_range < cols[1]-cols[0]+1:
+                largest_col_range = cols[1]-cols[0]+1
+                largest_col_range_id = tunnel_id, t_idx
+            if largest_z_range < zs[1]-zs[0]+1:
+                largest_z_range = zs[1]-zs[0]+1
+                largest_z_range_id = tunnel_id, t_idx
+        
+    print(f"Largest row range {largest_row_range} found for tunnel {largest_row_range_id[0]} at time slot {largest_row_range_id[1]}")
+    print(f"Largest col range {largest_col_range} found for tunnel {largest_col_range_id[0]} at time slot {largest_col_range_id[1]}")
+    print(f"Largest z range {largest_z_range} found for tunnel {largest_z_range_id[0]} at time slot {largest_z_range_id[1]}")
     return extracted_tunnels
 
 
-def main(input_folder: str, output_folder: str, padding: Tuple[int, int, int], overwrite_output_flag: bool = False) -> None:
+def main(input_folder: str, output_folder: str, overwrite_output_flag: bool = False) -> None:
     input_folder_path = Path(input_folder)
     output_folder_path = Path(output_folder)
 
     os.makedirs(output_folder_path, exist_ok=overwrite_output_flag)
     os.makedirs(output_folder_path / "GT", exist_ok=overwrite_output_flag)
     os.makedirs(output_folder_path / "IMG", exist_ok=overwrite_output_flag)
+    os.makedirs(output_folder_path / "GT_MERGED_LABELS", exist_ok=overwrite_output_flag)
 
     gt_path = input_folder_path / "01_GT" / "SEG"
     orig_path = input_folder_path / "01"
@@ -97,16 +122,14 @@ def main(input_folder: str, output_folder: str, padding: Tuple[int, int, int], o
     for i, (tunnel_gt, tunnel_img) in enumerate(extracted_tunnels):
         tifffile.imwrite(output_folder_path / "GT" / f"{i}.tif", tunnel_gt)
         tifffile.imwrite(output_folder_path / "IMG" / f"{i}.tif", tunnel_img)
+        tifffile.imwrite(output_folder_path / "GT_MERGED_LABELS" / f"{i}.tif", (tunnel_gt != 0).astype(np.bool))
 
 
 if __name__ == "__main__":
     import argparse
-
     parser = argparse.ArgumentParser(description="Split dataset images with padding.")
     parser.add_argument("input_folder", type=Path, help="Path to the folder containing images.")
     parser.add_argument("output_folder", type=Path, help="Path to the output folder.")
-    parser.add_argument("--padding", type=int, nargs=3, metavar=("PAD_X", "PAD_Y", "PAD_Z"),
-                        required=True, help="Padding for each axis (x, y, z) as three integers.")
     parser.add_argument('--output_overwrite', action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
     main(args.input_folder, args.output_folder, args.padding, args.output_overwrite)
