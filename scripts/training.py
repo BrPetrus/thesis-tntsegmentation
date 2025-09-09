08dcaf58727f3034e718207b9ecb6e0ba5835523
Bruno Petrus <xpetrus@fi.muni.cz>
2025-09-09 13:44:51 +0200

    feat: add zooming

--- scripts/training.py
+++ scripts/training.py
@@ -170,6 +170,9 @@ def _prepare_datasets(input_folder: Path, seed: int, validation_ratio = 1/3.) ->
         # Random rotations
         MT.RandRotated(keys=['volume', 'mask3d'], prob=0.5, range_x=0, range_y=0, range_z=np.pi/2),
 
+        # Random zoom
+        MT.RandZoomd(keys=['volume', 'mask3d'], prob=0.5, min_zoom=0.8, max_zoom=1.5),
+
         # # Elastic deformations
         # MT.Rand3DElasticd(
         #     keys=['volume', 'mask3d'],
