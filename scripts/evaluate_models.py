a2460f157050e61aa0686caecdc3307ba711eba7
Bruno Petrus <xpetrus@fi.muni.cz>
2025-09-15 17:07:48 +0200

    feat: implement tiled dataset and evaluation configuration for model inference

--- scripts/evaluate_models.py
+++ scripts/evaluate_models.py
@@ -3,6 +3,7 @@ import mlflow.artifacts
 import mlflow.client
 import mlflow.pytorch
 import mlflow.pytorch
+from monai.transforms.croppad import batch
 import torch
 import numpy as np
 import tifffile
@@ -15,18 +16,150 @@ import tempfile
 
 from typing import Tuple
 import torch.nn as nn
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, TensorDataset
 
 from config import ModelType
 from tilingutilities import AggregationMethod, stitch_volume, tile_volume
 from training_utils import create_neural_network
 from config import Config, ModelType
 
+from torch.utils.data import DataLoader
+
+from tntseg.utilities.dataset.datasets import TNTDataset, load_dataset_metadata
+import tqdm
+
+import monai.transforms as MT
+
+@dataclass
+class EvaluationConfig:
+    device: str = 'cpu'
+    crop_size: Tuple[int, int, int] = (7, 64, 64)
+    batch_size: int = 2
+    dataset_std: float = 0.07579
+    dataset_mean: float = 0.05988
+    shuffle: bool = False
+
+# Create a config instance
+config = EvaluationConfig()
+
+class TiledDataset(Dataset):
+    """Dataset that preserves tile positions with data."""
+    
+    def __init__(self, tiles_data, tiles_positions):
+        self.tiles_data = tiles_data
+        self.tiles_positions = tiles_positions
+        
+    def __len__(self):
+        return len(self.tiles_data)
+    
+    def __getitem__(self, idx):
+        return self.tiles_data[idx], self.tiles_positions[idx]
+
 def evaluate_model(nn: nn.Module, data: Dataset) -> None:
     pass
 
-def main(model: nn.Module, database_path: str, quadrant_idx: int, batch_size: int, device: str = 'cpu') -> None:
-    pass
+def main(model: nn.Module, database_path: str) -> None:
+    dataset = create_dataset(database_path)
+    print(f"Found {len(dataset)} images.")
+
+    # Split the data
+    # volume_tiles = []
+    # mask_tiles = []
+    for i in range(len(dataset)):
+        data, mask = dataset[i]
+        # Split the volume into tiles
+        tiles_data, tiles_positions = tile_volume(data[0], config.crop_size, overlap=0)
+
+        tiles_dataset = TiledDataset(tiles_data, tiles_positions)
+        tiles_dataloader = DataLoader(
+            tiles_dataset,
+            batch_size=config.batch_size,
+            shuffle=False
+        )
+
+        # Inference
+        all_predictions = []
+        all_positions = []
+        for batch_data, batch_positions in tqdm.tqdm(tiles_dataloader, desc="Running inference..."):
+            # Add fake colour channel
+            batch_data = batch_data[:, torch.newaxis, ...]
+
+            # Move data to device and keep positions on CPU
+            batch_data_dev = batch_data.to(config.device)
+            
+            # Run model
+            predictions = model(batch_data_dev).detach().to('cpu')
+            
+            # Store predictions and their positions
+            all_predictions.extend(predictions)
+            # all_positions.append(batch_positions)
+            # all_positions.extend([(d,r,c) for d,r,c in batch_positions])
+            # depths = [d for d,_,_ in batch_positions]
+            # rows = [r for _, r,_ in batch_positions]
+            # cols = [c for _, _, c in batch_positions]
+            depths = batch_positions[0]
+            rows = batch_positions[1]
+            cols = batch_positions[2]
+            all_positions.extend([(depths[i], rows[i], cols[i]) for i in range(len(batch_positions[0]))])
+            
+        # Convert to tensor for stitching
+        all_predictions_tensor = torch.stack(all_predictions)
+        
+        # Stitch the predictions back together using positions
+        reconstructed_volume = stitch_volume(
+            all_predictions_tensor, 
+            all_positions,
+            original_shape=data.shape[1:],
+            aggregation_method=AggregationMethod.Mean
+        )
+
+        # Threshold
+        thresholded = (reconstructed_volume.numpy() > 0.5).astype(np.uint8) * 255
+
+        # Save
+        save_path = Path(f"prediction_{i}.tif")
+        print(f"Saving prediction to {save_path.absolute()}")
+        tifffile.imwrite(save_path, reconstructed_volume.numpy())
+        save_path = Path(f"threshold_{i}.tif")
+        print(f"Saving threshold to {save_path.absolute()}")
+        tifffile.imwrite(save_path, thresholded)
+
+
+        # Evaluate
+
+
+    # # Evaluate model
+    # evaluate_model(nn, dataloader)
+
+    
+    
+
+def create_dataset(database_path: str | Path) -> Dataset: 
+    if isinstance(database_path, str):
+        database_path = Path(database_path)
+
+    # Load the dataset
+    df = load_dataset_metadata(database_path / "IMG", database_path / "GT_MERGED_LABELS")    
+
+    # Create the transforms
+    transforms = MT.Compose([
+        MT.NormalizeIntensityd(
+            keys=["volume"],
+            subtrahend=config.dataset_mean,
+            divisor=config.dataset_std
+        ),
+        # Convert to Tensor
+        MT.ToTensord(keys=['volume', 'mask3d']),
+    ])
+
+    # Create the dataset
+    dataset = TNTDataset(df, load_masks=True, transforms=transforms)
+    return dataset
+
+    # # create the dataloader
+    # dataloader = DataLoader(dataset, config.batch_size, config.shuffle)
+
+    # return dataloader
     
 def fetch_model_mlflow(client: mlflow.MlflowClient, experiment_name: str, run_name: str, model_path: str = 'model', device: str = 'cpu') -> nn.Module:
     experiment = client.get_experiment_by_name(experiment_name)
@@ -88,17 +221,14 @@ if __name__ == "__main__":
                         help='MLflow run name')
     parser.add_argument('database', type=str,
                         help='Path to database folder containing images')
-    parser.add_argument('quadrant', type=int,
-                        choices=[1, 2, 3, 4],
-                        help='Quadrant number to evaluate (1-4)')
     parser.add_argument('--experiment_name', type=str, default='Default',
                         help="Name of the MLFlow experiment")
     parser.add_argument('--mlflow_uri', type=str, default="http://127.0.0.1:8000",
                         help="MLFlow URI")
-    parser.add_argument('--batch_size', type=int, default=32,
-                        help="Batch size for inference")
-    parser.add_argument('--device', type=str, default='cpu',
-                        help="Device to run PyTorch on")
+    # parser.add_argument('--batch_size', type=int, default=32,
+    #                     help="Batch size for inference")
+    # parser.add_argument('--device', type=str, default='cpu',
+    #                     help="Device to run PyTorch on")
     # Add a parameter for local model path
     parser.add_argument('--local_model', type=str, default=None,
                         help="Path to a local model file (bypass MLflow)")
@@ -111,12 +241,12 @@ if __name__ == "__main__":
         # Set up MLFlow
         mlflow.set_tracking_uri(args.mlflow_uri)
         client = mlflow.MlflowClient(args.mlflow_uri)
-        model = fetch_model_mlflow(client, args.experiment_name, args.run_name, device=args.device)
+        model = fetch_model_mlflow(client, args.experiment_name, args.run_name)
     
     print('Model loaded successfully')
     
     # Run evaluation
-    main(model, args.database, args.quadrant, args.batch_size, args.device)
+    main(model, args.database)
 
 
 
--- scripts/tilingutilities.py
+++ scripts/tilingutilities.py
@@ -9,14 +9,16 @@ from torch.types import Tensor
 
 T = TypeVar('T', bound=np.generic)
 
+                # overlap: int = 0) -> List[Tuple[NDArray[T], int, int, int]]:
 def tile_volume(volume: NDArray[T], tile_size: Tuple[int, int, int],
-                overlap: int = 0) -> List[Tuple[NDArray[T], int, int, int]]:
+                overlap: int = 0) -> Tuple[NDArray[T], List[Tuple[int, int, int]]]:
     if volume.ndim != 3:
         raise ValueError("Expected 3D data")
     depth, rows, cols = volume.shape
     
     d_size, r_size, c_size = tile_size
     tiles = []
+    tiles_position = []
     
     for d in range(0, depth, d_size - overlap):
         for r in range(0, rows, r_size - overlap):
@@ -33,26 +35,29 @@ def tile_volume(volume: NDArray[T], tile_size: Tuple[int, int, int],
                 
                 # Extract tile and store with its position
                 tile = volume[d_start:d_end, r_start:r_end, c_start:c_end]
-                tiles.append((tile, d_start, r_start, c_start))
+                tiles.append(tile)
+                tiles_position.append((d_start, r_start, c_start))
     
-    return tiles
+    return tiles, tiles_position
 
 class AggregationMethod(StrEnum):
     Mean = auto()
     Max = auto()
     Min = auto()
 
-def stitch_volume(tiles: List[Tuple[NDArray[T], int, int, int]], 
-                    original_shape: Tuple[int, int, int], 
-                    overlap: int = 0,
-                    aggregation_method: AggregationMethod = AggregationMethod.Mean) -> NDArray[T]:
+def stitch_volume(tiles: List[torch.Tensor],
+                  positions: List[Tuple[int, int, int]], 
+                  original_shape: Tuple[int, int, int], 
+                  aggregation_method: AggregationMethod = AggregationMethod.Mean) -> NDArray[T]:
     # Initialize output volume and count matrix for averaging
     depth, rows, cols = original_shape
-    result = np.zeros(original_shape, dtype=tiles[0][0].dtype)
-    counts = np.zeros(original_shape, dtype=np.int32)
+    result = torch.zeros(original_shape, dtype=tiles[0].dtype)
+    counts = torch.zeros(original_shape, dtype=torch.int32)
 
     # Process each tile
-    for tile, d_start, r_start, c_start in tiles:
+    for idx, (d_start, r_start, c_start) in enumerate(positions):
+        tile = tiles[idx].squeeze(dim=0)
+
         d_end = d_start + tile.shape[0]
         r_end = r_start + tile.shape[1]
         c_end = c_start + tile.shape[2]
