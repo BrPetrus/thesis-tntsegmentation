9056274af4685436600faea884233946d5a55187
Bruno Petrus <xpetrus@fi.muni.cz>
2025-10-31 15:31:08 +0100

    aniso3d d=5

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@
 TIME=$((10*3600))
 echo "Setting time limit to $(($TIME / 3600)) hours"
 ulimit -t $TIME
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 echo "Currently visible CUDA capable devices: ${CUDA_VISIBLE_DEVICES}"
 
 # CLI option
@@ -22,7 +22,7 @@ MLFLOW_SERVER_PORT=8800
 LR=0.0001
 MODEL=anisotropicunet
 SEED=42
-MODEL_DEPTH=6
+MODEL_DEPTH=5
 WEIGHT_DECAY=0.0001
 HORIZONTAL_KERNEL="3,3,3"
 HORIZONTAL_PADDING="1,1,1"
